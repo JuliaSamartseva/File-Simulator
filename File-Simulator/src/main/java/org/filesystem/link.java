5a5be2a61957e4482eda871fe49d688bed77daee
Julia <yulia.samartseva@gmail.com>
2021-03-28 18:31:47 +0300

    implement Kernel.link() method

---
+++ File-Simulator/src/main/java/org/filesystem/link.java
@@ -0,0 +1,21 @@
+package org.filesystem;
+
+public class link {
+
+  public static String PROGRAM_NAME = "link";
+
+  public static void main(String[] argv) throws Exception {
+    Kernel.initialize();
+
+    if (argv.length != 2) {
+      System.err.println(
+              PROGRAM_NAME + " usage: java link <existing filepath> <new link filepath>");
+      Kernel.exit(1);
+    }
+
+    String existingFilepath = argv[0];
+    String linkFilepath = argv[1];
+
+    Kernel.link(existingFilepath, linkFilepath);
+  }
+}
